a141ef934c459a6393c9c3a423a4443b66c76a64
Jan Kvetina <jan.kvetina@gmail.com>
2022-07-03 20:45:05 +0200

    Clean tables dump

--- export_fn.py
+++ export_fn.py
@@ -35,3 +35,59 @@ def fix_simple_name(obj):
 
 
 
+def clean_table(lines):
+  lines[0] = fix_simple_name(lines[0]) + ' ('
+  lines[1] = lines[1].lstrip().lstrip('(').lstrip()  # fix fisrt column
+
+  # throw away some distrators
+  for (i, line) in enumerate(lines):
+    if line.startswith('  STORAGE') or\
+      line.startswith('  PCTINCREASE') or\
+      line.startswith('  BUFFER_POOL') or\
+      line.startswith('  USING'):
+      lines[i] = ''
+    else:
+      lines[i] = lines[i].replace(' ENABLE', '').strip()
+      lines[i] = lines[i].replace(' COLLATE "USING_NLS_COMP"', '')
+      lines[i] = lines[i].replace(' DEFAULT COLLATION "USING_NLS_COMP"', '')
+      lines[i] = lines[i].replace(' SEGMENT CREATION IMMEDIATE', '')
+      lines[i] = lines[i].replace('(PARTITION', '(\n    PARTITION')
+      lines[i] = lines[i].replace('" )', '"\n)')
+
+  # fix column alignment
+  lines = '\n'.join(lines).split('\n')
+  for (i, line) in enumerate(lines):
+    # fic column name
+    if line.startswith('"'):
+      columns = lines[i].strip().split(' ', 3)
+      lines[i] = '    {:<30}  {:<16}{}'.format(
+        fix_simple_name(columns[0]),
+        columns[1].replace('NUMBER(*,0)', 'INTEGER') if len(columns) > 1 else '',
+        fix_simple_name(' '.join(columns[2:])) if len(columns) > 2 else ''
+      ).rstrip()
+    #
+    if line.startswith(')  DEFAULT COLLATION "USING_NLS_COMP" PCTFREE'):
+      lines[i] = ')'
+    #
+    if line.startswith('TABLESPACE') or\
+      line.startswith('PCTFREE') or\
+      line.startswith('NOCOMPRESS LOGGING'):
+      lines[i] = ''
+    #
+    if line.startswith('CONSTRAINT'):
+      lines[i] = '    --\n    ' + fix_simple_name(lines[i])
+      lines[i] = lines[i].replace(' CHECK (', '\n        CHECK (')
+      lines[i] = lines[i].replace(' PRIMARY KEY (', '\n        PRIMARY KEY (')
+      lines[i] = lines[i].replace(' FOREIGN KEY (', '\n        FOREIGN KEY (')
+      lines[i] = lines[i].replace(' UNIQUE (', '\n        UNIQUE (')
+    #
+    if line.startswith('REFERENCES'):
+      lines[i] = '        ' + fix_simple_name(lines[i])
+
+  # remove empty lines
+  lines = list(filter(None, lines))
+  lines[len(lines) - 1] += ';'
+  return lines
+
+
+
