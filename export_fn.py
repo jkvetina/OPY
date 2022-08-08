b4417cd54ce74227e09e804784ce114019505f25
Jan Kvetina <jan.kvetina@gmail.com>
2022-08-08 11:05:12 +0200

    Fix path issues

--- export_fn.py
+++ export_fn.py
@@ -6,7 +6,7 @@ from export_queries import *
 def get_file_details(file, git_root, hashed_old):
   short_file  = file.replace(git_root, '').replace('\\', '/').lstrip('/')
   hash_old    = hashed_old.get(short_file, '')
-  hash_new    = hashlib.md5(open(file, 'rb').read()).hexdigest()
+  hash_new    = hashlib.md5(open(os.path.normpath(file), 'rb').read()).hexdigest()
   #
   return (short_file, hash_old, hash_new)
 
