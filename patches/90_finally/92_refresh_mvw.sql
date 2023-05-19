8a32e1b1312df9db8620496b2cc5a6a68712ea5c
Jan Kvetina <jan.kvetina@gmail.com>
2023-05-19 15:25:49 +0200

    Provide better script to refresh MViews

--- patches/90_finally/92_refresh_mvw.sql
+++ patches/90_finally/92_refresh_mvw.sql
@@ -1,4 +1,26 @@
+DECLARE
+    v_start     DATE;
 BEGIN
-    DBMS_MVIEW.REFRESH_ALL_MVIEWS();
+    FOR c IN (
+        SELECT m.mview_name
+        FROM all_mviews m
+        WHERE m.owner           = USER                  -- adjust
+            AND m.mview_name    LIKE '%' ESCAPE '\'     -- adjust
+        ORDER BY 1
+    ) LOOP
+        v_start := SYSDATE;
+        --
+        DBMS_OUTPUT.PUT_LINE('--');
+        DBMS_OUTPUT.PUT_LINE('-- REFRESHING ' || c.mview_name);
+        --
+        DBMS_MVIEW.REFRESH (
+            list    => c.mview_name,
+            method  => 'C'
+        );
+        --
+        DBMS_OUTPUT.PUT_LINE('--   DONE ' || CEIL((SYSDATE - v_start) * 86400) || 's');
+        DBMS_OUTPUT.PUT_LINE('--');
+        DBMS_OUTPUT.PUT_LINE('');
+    END LOOP;
 END;
 /
