3b0245685a2e2aafd96526d2556f6b9d842eb44d
Jan Kvetina <jan.kvetina@gmail.com>
2023-09-06 11:38:35 +0200

    Better care about indexes

--- patches/90_finally/94_indexes.sql
+++ patches/90_finally/94_indexes.sql
@@ -9,12 +9,65 @@ BEGIN
         WHERE i.index_type          LIKE '%NORMAL%'
             AND i.tablespace_name   != in_target_tablespace
     ) LOOP
-        DBMS_OUTPUT.PUT_LINE(c.table_name || ' ->' || c.index_name);
-        EXECUTE IMMEDIATE
-            'ALTER INDEX ' || c.index_name ||
-            ' REBUILD TABLESPACE ' || in_target_tablespace;
+        BEGIN
+            DBMS_OUTPUT.PUT_LINE(RPAD(c.table_name, ' ', 30) || ' -> ' || c.index_name);
+            EXECUTE IMMEDIATE
+                'ALTER INDEX ' || c.index_name ||
+                ' REBUILD TABLESPACE ' || in_target_tablespace;
+        EXCEPTION
+        WHEN OTHERS THEN
+            NULL;
+        END;
     END LOOP;
 END;
 /
 */
+
+-- create missing FK indexes
+DECLARE
+    in_prefix       CONSTANT VARCHAR2(30) := 'XX%';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('--');
+    DBMS_OUTPUT.PUT_LINE('-- MISSING INDEXES:');
+    DBMS_OUTPUT.PUT_LINE('--');
+    --
+    FOR c IN (
+        WITH f AS (
+            SELECT
+                t.table_name,
+                t.constraint_name AS index_name,
+                LISTAGG(t.column_name, ', ') WITHIN GROUP (ORDER BY t.position) AS cols
+            FROM user_cons_columns t
+            JOIN user_constraints n
+                ON n.constraint_name    = t.constraint_name
+            WHERE n.constraint_type     = 'R'
+                AND t.table_name        LIKE in_prefix || '%' ESCAPE '\'
+            GROUP BY t.table_name, t.constraint_name
+        )
+        SELECT
+            f.*, i.index_name AS existing_index, i.cols AS index_cols,
+            --
+            'CREATE INDEX ' || RPAD(f.index_name, 30) ||
+                ' ON ' || RPAD(f.table_name, 30) || ' (' || f.cols || ') COMPUTE STATISTICS' AS fix
+        FROM f
+        LEFT JOIN (
+            SELECT i.table_name, i.index_name, LISTAGG(i.column_name, ', ') WITHIN GROUP (ORDER BY i.column_position) AS cols
+            FROM user_ind_columns i
+            GROUP BY i.table_name, i.index_name
+        ) i
+            ON i.table_name     = f.table_name
+            AND i.cols          LIKE f.cols || '%'
+        WHERE i.index_name      IS NULL  -- show only missing indexes
+        ORDER BY 1, 2
+    ) LOOP
+        DBMS_OUTPUT.PUT_LINE(c.fix || ';');
+        --EXECUTE IMMEDIATE c.fix;
+    END LOOP;
+    --
+    DBMS_OUTPUT.PUT_LINE('');
+END;
+/
+
+
+
 
