e653d27c2ce778a26350b1db860279ba7d2b98c0
jan.k@remparsolutions.com <jan.kvetina@gmail.com>
2023-11-17 09:05:54 +0100

    Remove noneditionable thingy, rely on default

--- export_fn.py
+++ export_fn.py
@@ -346,7 +346,7 @@ def clean_view(object_name, lines, schema, cfg):
   if len(lines) == 0:
     return []
   lines[0] = lines[0].replace(' DEFAULT COLLATION "USING_NLS_COMP"', '')
-  lines[0] = lines[0].replace(' EDITIONABLE', '')
+  lines[0] = lines[0].replace(' EDITIONABLE', '').replace(' NONEDITIONABLE', '')
   lines[0] = replace(lines[0], r'\s*\([^)]+\)\s*AS', ' AS')                 # remove columns
   lines[0] = replace(lines[0], r'\s*\([^)]+\)\s*BEQUEATH', ' BEQUEATH')     # remove columns
   lines[0] = lines[0].replace(' ()  AS', ' AS')                             # fix some views
@@ -446,7 +446,7 @@ def clean_package(object_name, lines, schema, cfg):
 
   # remove body
   for (i, line) in enumerate(lines):
-    if line.replace(' EDITIONABLE', '').startswith('CREATE OR REPLACE PACKAGE BODY'):
+    if line.replace(' EDITIONABLE', '').replace(' NONEDITIONABLE', '').startswith('CREATE OR REPLACE PACKAGE BODY'):
       lines = lines[0:i]
       lines[len(lines) - 1] += '\n/'
       break
@@ -463,7 +463,7 @@ def clean_package_body(object_name, lines, schema, cfg):
 def clean_procedure(object_name, lines, schema, cfg):
   if len(lines):
     lines[0] = fix_simple_name(lines[0], schema)
-    lines[0] = lines[0].replace(' EDITIONABLE', '')
+    lines[0] = lines[0].replace(' EDITIONABLE', '').replace(' NONEDITIONABLE', '')
     lines[len(lines) - 1] += '\n/'
   return lines
 
@@ -508,7 +508,7 @@ def clean_trigger(object_name, lines, schema, cfg):
     return lines
   #
   lines[0] = fix_simple_name(lines[0], schema)
-  lines[0] = lines[0].replace(' EDITIONABLE', '')
+  lines[0] = lines[0].replace(' EDITIONABLE', '').replace(' NONEDITIONABLE', '')
 
   # fix enable/disable trigger
   found_slash = False
@@ -553,7 +553,7 @@ def clean_index(object_name, lines, schema, cfg):
 
 def clean_synonym(object_name, lines, schema, cfg):
   if len(lines):
-    lines[0] = lines[0].replace(' EDITIONABLE', '')
+    lines[0] = lines[0].replace(' EDITIONABLE', '').replace(' NONEDITIONABLE', '')
     lines[0] = fix_simple_name(lines[0], schema)
     lines[len(lines) - 1] += ';'
   #
